943d475bc2e045380461beff66f9acf59b2aa762
shubham kesri <shubhamkeshari90@gmail.com>
2022-04-06 16:38:02 +0530

    ADDED SEtter for token

--- Text.java
+++ Text.java
@@ -3,7 +3,11 @@ public class Text {
 private String token;
 
 	Text(String token){
-       this.token = token;
+    //    this.token = token;
+	   setString(token);	
 	}
 
+	public void setString(String token){
+		this.token = token;
+	}
 }
